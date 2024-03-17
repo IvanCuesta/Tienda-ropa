a3310225ae2c3edc79ff7a99d4f50c446ec4e7d9
Ivan <ivanetflix977@gmail.com>
2024-03-17 19:05:03 +0100

    Conexion a la base de datos

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,11 +1,30 @@
-export const HOST = "localhost";
-export const USER = "tu_usuario";
-export const PASSWORD = "tu_contraseña";
-export const DB = "tu_base_de_datos";
-export const dialect = "mysql";
-export const pool = {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-};
+import { Sequelize } from 'sequelize';
+
+export const dbConfig = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "1234",
+    DB: "tienda_ropa",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+
+
+
+  const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+  
+  export default db ;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 import app from './app.js';
+import db from './config/db.config.js';
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`El servidor está corriendo en el puerto ${PORT}.`);
-});
+
+
+// Probar la conexión a la base de datos antes de iniciar el servidor
+db.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida exitosamente.');
+    // Iniciar el servidor solo si la conexión a la base de datos es exitosa
+    app.listen(PORT, () => {
+      console.log(`El servidor está corriendo en: http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
